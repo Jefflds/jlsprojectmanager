4f78c09383ceb9473c92ec34e9f44fca012c7ede
Jefferson Laurindo da Silva <107891837+Jefflds@users.noreply.github.com>
2023-07-12 19:16:43 -0300

    Estruturacao do projeto.

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,36 @@
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-import Home from './components/pages/Home';
-import Company from './components/pages/Company';
-import Contact from './components/pages/Contact';
-import NewProject from './components/pages/NewProject';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./components/pages/Home";
+import Company from "./components/pages/Company";
+import Contact from "./components/pages/Contact";
+import NewProject from "./components/pages/NewProject";
+import Container from "./components/layout/Container";
 
 function App() {
   return (
     <Router>
-      <ul>
-        <li>Home</li>
-        <li>Contato</li>
-      </ul>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/company">Empresa</Link>
+        <Link to="/contact">Contato</Link>
+        <Link to="/newproject">Novo Projeto</Link>
+      </div>
       <Switch>
-        <Route exact path='/'>
-          <Home/>
-        </Route>
-        <Route exact path='/company'>
-          <Company/>
-        </Route>
-        <Route exact path='/contact'>
-          <Contact/>
-        </Route>
-        <Route exact path='/newproject'>
-          <NewProject/>
-        </Route>
+        <Container customClass="min-height">
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/company">
+            <Company />
+          </Route>
+          <Route exact path="/contact">
+            <Contact />
+          </Route>
+          <Route exact path="/newproject">
+            <NewProject />
+          </Route>
+        </Container>
       </Switch>
+      <p>Footer</p>
     </Router>
   );
 }
