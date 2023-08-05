bf4d8642663863a42d2773c9716df12d8619b908
Jefferson Laurindo da Silva <107891837+Jefflds@users.noreply.github.com>
2023-08-04 21:58:00 -0300

    Criacao da pagina Home e do botao de link

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,5 +1,17 @@
+import Styles from './Home.module.css';
+import savings from '../../img/savings.svg'
+import LinkButton from '../layout/LinkButton';
+
 function Home(){
-    return <h1>Home</h1>
+    return (
+        <section className={Styles.home_container}>
+            <h1>Seja Bem-Vindo ao <span>JLS Project Manager</span></h1>
+            <p>Comece a gerenciar os seus projetos agora mesmo!</p>
+            <LinkButton to="/newproject" text="Criar Projeto"/>
+            <br/>
+            <img src={savings} alt="Imagem de um porco que remete ao dinheiro" />
+        </section>
+    )
 }
 
 export default Home
